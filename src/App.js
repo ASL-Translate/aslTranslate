addb33dfff6a84005adea1c0ea04d893a7ff3c46
ManOfNeptune27 <cbrunne8@kent.edu>
2025-02-15 13:48:20 -0500

    starting point

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import SearchDictionary from "./SearchDictionary";
+
+function App() {
+  return (
+    <div className="flex justify-center items-center min-h-screen">
+      <SearchDictionary />
+    </div>
+  );
+}
+
+export default App;
